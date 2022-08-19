f80a919166e3e2c4d546adafb7db539b3cf5cf26
Cleudeir <Cleudeirsilva@gmail.com>
2022-08-19 17:27:30 -0300

    adicionar gestores

--- src/Class/db.ts
+++ src/Class/db.ts
@@ -75,12 +75,6 @@ class db {
         
         try {
             const [result] : any[] = await (await this.mysql).query(queryString)
-            if(result.length  === 0 ){
-                return {sqlMessage:"Nada encontrado"}
-            }
-            if (Object.keys(console).length === 0) {
-                return {sqlMessage: "Nada encontrado"};
-              }
             return result
         } catch (error) {
             if(error.sqlMessage){
